7d0e791e607336a04b7df3d0aeb88ce81ca71412
Wolfgang Maier <maierw@informatik.uni-freiburg.de>
2021-03-23 17:17:34 +0100

    Parameterize new history name

--- run_beaconize.sh
+++ run_beaconize.sh
@@ -2,6 +2,7 @@
 BOT_TAG='bot-go-beaconize'
 BOT_RESPONSE='beacon-bot-ok'
 DEST_TAG='beacon-export'
+DEST_NAME_SUFFIX='Viral Beacon export'
 BAM_DATA='Fully processed reads for variant calling (primer-trimmed, realigned reads with added indelquals)'
 VCF_DATA='Final (SnpEff-) annotated variants'
 JOB_YML='beacon_export-job.yml'
@@ -25,7 +26,7 @@ if [ -s "$WORKDIR/$JOB_YML" ]; then
     # wait for successful completion of workflow scheduling by planemo run, then tag the new history and retag the source history
     (while [ ! -s "$WORKDIR/run_info.txt" ]; do sleep 60; done; DEST_HISTORY_ID=$(grep -m1 -o 'histories/[^?]*' "$WORKDIR/run_info.txt" | cut -d / -f 2) && python bioblend-scripts/tag_history.py $DEST_HISTORY_ID -g "https://usegalaxy.eu" -a $API_KEY -t $DEST_TAG && python bioblend-scripts/tag_history.py $SOURCE_HISTORY_ID -g "https://usegalaxy.eu" -a $API_KEY -t $BOT_RESPONSE -r $BOT_TAG) &
     # run the viral beacon WF
-    planemo -v run d4b0444778f0519c "$WORKDIR/$JOB_YML" --history_name "$SOURCE_HISTORY_NAME"" Viral Beacon export" --galaxy_url 'https://usegalaxy.eu' --galaxy_user_key $API_KEY --engine external_galaxy 2>&1 > /dev/null | grep -o 'GET /api/histories/[^?]*\?' > "$WORKDIR/run_info.txt"
+    planemo -v run d4b0444778f0519c "$WORKDIR/$JOB_YML" --history_name "$SOURCE_HISTORY_NAME - $DEST_NAME_SUFFIX" --galaxy_url 'https://usegalaxy.eu' --galaxy_user_key $API_KEY --engine external_galaxy 2>&1 > /dev/null | grep -o 'GET /api/histories/[^?]*\?' > "$WORKDIR/run_info.txt"
 fi
 
 rm -R $WORKDIR
