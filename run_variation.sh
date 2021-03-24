3c575334e21a4748cd9f38a972d9754972c1dcd9
Simon Bray <sbray1371@gmail.com>
2021-03-24 12:54:38 +0100

    add check_history stage to run_variation.sh

--- run_variation.sh
+++ run_variation.sh
@@ -1,9 +1,9 @@
+VARIATION_WF_ID='b9358bbbe4cec7fe'
+
 # check previous invocation
+PREVIOUS_HISTORY=`planemo list_invocations $VARIATION_WF_ID --profile local | head -n 4 | tail -c 17`
+python bioblend-scripts/check_history.py -g https://usegalaxy.eu -a $API_KEY -p 0.67 --history_id ${PREVIOUS_HISTORY}  # check history"
 
-planemo list_invocations ...
-
-python ...  # check history
-
-if ...
+if [ $? -eq 0 ]; then
   planemo run ...
 fi
