f89553bd089cf80413aa84375120ce0519047199
Simon Bray <32272674+simonbray@users.noreply.github.com>
2021-03-26 12:00:54 +0100

    Add check_history stage to run_variation.sh (#7)
    * add check_history stage to run_variation.sh
    
    * add get_most_recent_history_by_tag.py script

--- run_variation.sh
+++ run_variation.sh
@@ -1,9 +1,10 @@
+VARIATION_WF_ID='b9358bbbe4cec7fe'
+VARIATION_TAG='variation'
+
 # check previous invocation
+PREVIOUS_HISTORY=`python bioblend-scripts/get_most_recent_history_by_tag.py -g https://usegalaxy.eu -a $API_KEY --tag $VARIATION_TAG`
+python bioblend-scripts/check_history.py -g https://usegalaxy.eu -a $API_KEY -p 0.67 --history_id ${PREVIOUS_HISTORY}  # check history"
 
-planemo list_invocations ...
-
-python ...  # check history
-
-if ...
+if [ $? -eq 0 ]; then
   planemo run ...
 fi
