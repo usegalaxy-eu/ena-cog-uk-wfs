1867783a5fa40cfd9a8c4eb31c5ed19c024f11e8
Wolfgang Maier <maierw@informatik.uni-freiburg.de>
2021-03-27 18:26:02 +0100

    Encode target instance & WF ID in job template
    With this change the job template files become the only place that needs
    to be edited in order to port the bots to a different Galaxy instance.
    
    Also remove bot tags of downstream bots as early as possible to avoid
    reprocessing of the same variation history.

--- run_beaconize.sh
+++ run_beaconize.sh
@@ -9,24 +9,27 @@ JOB_YML='beacon_export-job.yml'
 
 # common for all bots
 JOB_YML_DIR='job-yml-templates'
+GALAXY_SERVER=$(grep '#use_server:' "$JOB_YML_DIR/$JOB_YML" | cut -d ' ' -f 2-)
+WF_ID=$(grep '#use_workflow_id:' "$JOB_YML_DIR/$JOB_YML" | cut -d ' ' -f 2-)
 
 # start processing
 WORKDIR=$BOT_TAG'_run_'$(date '+%s')
 mkdir $WORKDIR &&
 
-#generate the job.yml needed by planemo run
-cat "$JOB_YML_DIR/$JOB_YML" | python bioblend-scripts/find_datasets.py "$BAM_DATA" "$VCF_DATA" -g "https://usegalaxy.eu" -a $API_KEY -t $BOT_TAG --collections-only -n 1 --from-template -o "$WORKDIR/$JOB_YML"
+# generate the job.yml needed by planemo run from its template
+cat "$JOB_YML_DIR/$JOB_YML" | python bioblend-scripts/find_datasets.py "$BAM_DATA" "$VCF_DATA" -g "$GALAXY_SERVER" -a $API_KEY -t $BOT_TAG --collections-only -n 1 --from-template -o "$WORKDIR/$JOB_YML"
 
-# TO DO: remove $BOT_TAG from history - as part of find_datasets.py or via separate script
 if [ -s "$WORKDIR/$JOB_YML" ]; then
     # if the formatted job.yml file contains data, we know a suitable source history was found
     # otherwise we assume no history is ready for processing
     SOURCE_HISTORY_ID=$(grep '#from_history_id:' "$WORKDIR/$JOB_YML" | cut -d ' ' -f 2-)
     SOURCE_HISTORY_NAME=$(grep '#from_history_name:' "$WORKDIR/$JOB_YML" | cut -d ' ' -f 2-)
+    # prevent reprocessing of the same history by removing its bot-specific tag
+    python bioblend-scripts/tag_history.py $SOURCE_HISTORY_ID -g "$GALAXY_SERVER" -a $API_KEY -r $BOT_TAG
     # wait for successful completion of workflow scheduling by planemo run, then tag the new history and retag the source history
-    (while [ ! -s "$WORKDIR/run_info.txt" ]; do sleep 60; done; DEST_HISTORY_ID=$(grep -m1 -o 'histories/[^?]*' "$WORKDIR/run_info.txt" | cut -d / -f 2) && python bioblend-scripts/tag_history.py $DEST_HISTORY_ID -g "https://usegalaxy.eu" -a $API_KEY -t $DEST_TAG && python bioblend-scripts/tag_history.py $SOURCE_HISTORY_ID -g "https://usegalaxy.eu" -a $API_KEY -t $BOT_RESPONSE -r $BOT_TAG) &
+    (while [ ! -s "$WORKDIR/run_info.txt" ]; do sleep 60; done; DEST_HISTORY_ID=$(grep -m1 -o 'histories/[^?]*' "$WORKDIR/run_info.txt" | cut -d / -f 2) && python bioblend-scripts/tag_history.py $DEST_HISTORY_ID -g "$GALAXY_SERVER" -a $API_KEY -t $DEST_TAG && python bioblend-scripts/tag_history.py $SOURCE_HISTORY_ID -g "$GALAXY_SERVER" -a $API_KEY -t $BOT_RESPONSE) &
     # run the viral beacon WF
-    planemo -v run d4b0444778f0519c "$WORKDIR/$JOB_YML" --history_name "$SOURCE_HISTORY_NAME - $DEST_NAME_SUFFIX" --galaxy_url 'https://usegalaxy.eu' --galaxy_user_key $API_KEY --engine external_galaxy 2>&1 > /dev/null | grep -o 'GET /api/histories/[^?]*\?' > "$WORKDIR/run_info.txt"
+    planemo -v run $WF_ID "$WORKDIR/$JOB_YML" --history_name "$SOURCE_HISTORY_NAME - $DEST_NAME_SUFFIX" --galaxy_url "$GALAXY_SERVER" --galaxy_user_key $API_KEY --engine external_galaxy 2>&1 > /dev/null | grep -o 'GET /api/histories/[^?]*\?' > "$WORKDIR/run_info.txt"
 fi
 
 rm -R $WORKDIR
--- run_variation.sh
+++ run_variation.sh
@@ -5,20 +5,21 @@ DEST_TAG='cog-uk_variation'
 DEST_BOT_TAGs='bot-go-report bot-go-consensus bot-go-beaconize'
 JOB_YML='variation-job.yml'
 DOWNLOADED_DATA_COLLECTION='Paired Collection'
-WF_ID='48509fef29ad8166'
 
 # common for all bots
 JOB_YML_DIR='job-yml-templates'
+GALAXY_SERVER=$(grep '#use_server:' "$JOB_YML_DIR/$JOB_YML" | cut -d ' ' -f 2-)
+WF_ID=$(grep '#use_workflow_id:' "$JOB_YML_DIR/$JOB_YML" | cut -d ' ' -f 2-)
 
 # start processing
 WORKDIR=$DEST_TAG'_run_'$(date '+%s')
 mkdir $WORKDIR &&
 
 # check progress of previous invocation
-PREVIOUS_HISTORY=`python bioblend-scripts/get_most_recent_history_by_tag.py -g https://usegalaxy.eu -a $API_KEY --tag $DEST_TAG`
-python bioblend-scripts/check_history.py -g https://usegalaxy.eu -a $API_KEY -p 0.67 --dataset-marker 'Final (SnpEff-) annotated variants' $PREVIOUS_HISTORY &&
-# start building the job.yml needed by planemo run
-cat "$JOB_YML_DIR/$JOB_YML" | python bioblend-scripts/find_collection_elements.py "FTP links by library" -g "https://usegalaxy.eu" -a $API_KEY -t cog-uk_ena-meta -n 1 --from-template -o "$WORKDIR/$JOB_YML"
+PREVIOUS_HISTORY=`python bioblend-scripts/get_most_recent_history_by_tag.py -g "$GALAXY_SERVER" -a $API_KEY --tag $DEST_TAG`
+python bioblend-scripts/check_history.py -g "$GALAXY_SERVER" -a $API_KEY -p 0.67 --dataset-marker 'Final (SnpEff-) annotated variants' $PREVIOUS_HISTORY &&
+# start building the job.yml needed by planemo run from its template
+cat "$JOB_YML_DIR/$JOB_YML" | python bioblend-scripts/find_collection_elements.py "FTP links by library" -g "$GALAXY_SERVER" -a $API_KEY -t cog-uk_ena-meta -n 1 --from-template -o "$WORKDIR/$JOB_YML"
 
 if [ -s "$WORKDIR/$JOB_YML" ]; then
     # if after the above server queries a job yml file exists in WORKDIR and that file contains data, we know it's ok to proceed
@@ -28,18 +29,19 @@ if [ -s "$WORKDIR/$JOB_YML" ]; then
     ENA_LINKS=$(grep '#from_ena_links_in:' "$WORKDIR/$JOB_YML" | cut -d ' ' -f 2-)
     DEST_NAME_SUFFIX=$(grep '#batch_name:' "$WORKDIR/$JOB_YML" | cut -d ' ' -f 2-)
     # add information about the collection to be built from downloaded data to the job yml file
-    python bioblend-scripts/ftp_links_to_yaml.py $ENA_LINKS "$DOWNLOADED_DATA_COLLECTION" -g "https://usegalaxy.eu" -a $API_KEY >> "$WORKDIR/$JOB_YML"
+    python bioblend-scripts/ftp_links_to_yaml.py $ENA_LINKS "$DOWNLOADED_DATA_COLLECTION" -g "$GALAXY_SERVER" -a $API_KEY >> "$WORKDIR/$JOB_YML"
 
     # wait for successful completion of workflow scheduling by planemo run, then retag the source history ENA links dataset
-    (while [ ! -s "$WORKDIR/run_info.txt" ]; do sleep 60; done; DEST_HISTORY_ID=$(grep -m1 -o 'histories/[^?]*' "$WORKDIR/run_info.txt" | cut -d / -f 2) &&  python bioblend-scripts/tag_history.py $SOURCE_HISTORY_ID --dataset-id $ENA_LINKS -g "https://usegalaxy.eu" -a $API_KEY -t $BOT_RESPONSE -r $BOT_RESERVE) &
+    (while [ ! -s "$WORKDIR/run_info.txt" ]; do sleep 60; done; DEST_HISTORY_ID=$(grep -m1 -o 'histories/[^?]*' "$WORKDIR/run_info.txt" | cut -d / -f 2) &&  python bioblend-scripts/tag_history.py $SOURCE_HISTORY_ID --dataset-id $ENA_LINKS -g "$GALAXY_SERVER" -a $API_KEY -t $BOT_RESPONSE -r $BOT_RESERVE) &
     # put a bot reserved tag on the input dataset to prevent it from being processed again
     # this will be changed to a processed tag by the background job launched above
-    python bioblend-scripts/tag_history.py $SOURCE_HISTORY_ID --dataset-id $ENA_LINKS -g "https://usegalaxy.eu" -a $API_KEY -t $BOT_RESERVE &&
+    python bioblend-scripts/tag_history.py $SOURCE_HISTORY_ID --dataset-id $ENA_LINKS -g "$GALAXY_SERVER" -a $API_KEY -t $BOT_RESERVE &&
     # run the WF
-    planemo -v run $WF_ID "$WORKDIR/$JOB_YML" --history_name "COG-UK $DEST_NAME_SUFFIX" --tags $DEST_TAG --galaxy_url 'https://usegalaxy.eu' --galaxy_user_key $API_KEY --engine external_galaxy 2>&1 > /dev/null | grep -o 'GET /api/histories/[^?]*\?' > "$WORKDIR/run_info.txt" &&
+    planemo -v run $WF_ID "$WORKDIR/$JOB_YML" --history_name "COG-UK $DEST_NAME_SUFFIX" --tags $DEST_TAG --galaxy_url "$GALAXY_SERVER" --galaxy_user_key $API_KEY --engine external_galaxy 2>&1 > /dev/null | grep -o 'GET /api/histories/[^?]*\?' > "$WORKDIR/run_info.txt" &&
     # on successful completion of the WF invocation inform downstream bots
+    # by tagging the new history accordingly
     DEST_HISTORY_ID=$(grep -m1 -o 'histories/[^?]*' "$WORKDIR/run_info.txt" | cut -d / -f 2) &&
-    python bioblend-scripts/tag_history.py $DEST_HISTORY_ID -g "https://usegalaxy.eu" -a $API_KEY -t $DEST_BOT_TAGs
+    python bioblend-scripts/tag_history.py $DEST_HISTORY_ID -g "$GALAXY_SERVER" -a $API_KEY -t $DEST_BOT_TAGs
 fi
 
 rm -R $WORKDIR
