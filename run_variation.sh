57f0576ca3736d0618c2b071e0657e31df5b4120
Wolfgang Maier <maierw@informatik.uni-freiburg.de>
2021-03-28 23:48:57 +0200

    Make run_variation work with released planemo
    Instead of requiring immediate tagging of the destination history by
    planemo run (currently only available from the master branch), use a check for
    a bot-scheduling tag on any ENA links input to prevent additional workflow
    invocations.
    Only when bot-scheduling gets replaced by bot-processing is the status of the
    then known destination history of importance.

--- run_variation.sh
+++ run_variation.sh
@@ -1,6 +1,7 @@
 # bot-specific settings
-BOT_RESERVE='bot-processing'
-BOT_RESPONSE='bot-processed'
+BOT_SIGNAL1='bot-scheduling'
+BOT_SIGNAL2='bot-processing'
+BOT_SIGNAL3='bot_processed'
 DEST_TAG='cog-uk_variation'
 DEST_BOT_TAGs='bot-go-report bot-go-consensus bot-go-beaconize'
 JOB_YML='variation-job.yml'
@@ -16,11 +17,14 @@ WORKDIR=$DEST_TAG'_run_'$(date '+%s')
 mkdir $WORKDIR &&
 
 # check progress of previous invocation
-PREVIOUS_HISTORY=`python bioblend-scripts/get_most_recent_history_by_tag.py -g "$GALAXY_SERVER" -a $API_KEY --tag $DEST_TAG`
-python bioblend-scripts/check_history.py -g "$GALAXY_SERVER" -a $API_KEY -p 0.67 --dataset-marker 'Final (SnpEff-) annotated variants' $PREVIOUS_HISTORY &&
-# start building the job.yml needed by planemo run from its template
-cat "$JOB_YML_DIR/$JOB_YML" | python bioblend-scripts/find_collection_elements.py "FTP links by library" -g "$GALAXY_SERVER" -a $API_KEY -t cog-uk_ena-meta -n 1 --from-template -o "$WORKDIR/$JOB_YML"
-
+SCHEDULING=$(echo "{collections[0][elements][0][element_identifier]}" | python bioblend-scripts/find_collection_elements.py "FTP links by library" -g "$GALAXY_SERVER" -a $API_KEY -t cog-uk_ena-meta -c $BOT_SIGNAL1 -n 1 --from-template)
+if [ -z "$SCHEDULING" ]; then
+    # no scheduling WF invocation found => check processing invocations if the latest has progressed far enough
+    PREVIOUS_HISTORY=`python bioblend-scripts/get_most_recent_history_by_tag.py -g "$GALAXY_SERVER" -a $API_KEY --tag $DEST_TAG`
+    python bioblend-scripts/check_history.py -g "$GALAXY_SERVER" -a $API_KEY -p 0.67 $PREVIOUS_HISTORY &&
+    # start building the job.yml needed by planemo run from its template
+    cat "$JOB_YML_DIR/$JOB_YML" | python bioblend-scripts/find_collection_elements.py "FTP links by library" -g "$GALAXY_SERVER" -a $API_KEY -t cog-uk_ena-meta -n 1 --from-template -o "$WORKDIR/$JOB_YML"
+fi
 if [ -s "$WORKDIR/$JOB_YML" ]; then
     # if after the above server queries a job yml file exists in WORKDIR and that file contains data, we know it's ok to proceed
     # otherwise we assume no action should be taken this time
@@ -31,17 +35,19 @@ if [ -s "$WORKDIR/$JOB_YML" ]; then
     # add information about the collection to be built from downloaded data to the job yml file
     python bioblend-scripts/ftp_links_to_yaml.py $ENA_LINKS "$DOWNLOADED_DATA_COLLECTION" -g "$GALAXY_SERVER" -a $API_KEY >> "$WORKDIR/$JOB_YML"
 
-    # wait for successful completion of workflow scheduling by planemo run, then retag the source history ENA links dataset
-    (while [ ! -s "$WORKDIR/run_info.txt" ]; do sleep 60; done; DEST_HISTORY_ID=$(grep -m1 -o 'histories/[^?]*' "$WORKDIR/run_info.txt" | cut -d / -f 2) &&  python bioblend-scripts/tag_history.py $SOURCE_HISTORY_ID --dataset-id $ENA_LINKS -g "$GALAXY_SERVER" -a $API_KEY -t $BOT_RESPONSE -r $BOT_RESERVE) &
+    # wait for successful completion of workflow scheduling by planemo run, then tag new history and retag the source history ENA links dataset
+    (while [ ! -s "$WORKDIR/run_info.txt" ]; do sleep 60; done; DEST_HISTORY_ID=$(grep -m1 -o 'histories/[^?]*' "$WORKDIR/run_info.txt" | cut -d / -f 2) && python bioblend-scripts/tag_history.py $DEST_HISTORY_ID -g "$GALAXY_SERVER" -a $API_KEY -t $DEST_TAG && python bioblend-scripts/tag_history.py $SOURCE_HISTORY_ID --dataset-id $ENA_LINKS -g "$GALAXY_SERVER" -a $API_KEY -t $BOT_SIGNAL2 -r $BOT_SIGNAL1) &
     # put a bot reserved tag on the input dataset to prevent it from being processed again
     # this will be changed to a processed tag by the background job launched above
-    python bioblend-scripts/tag_history.py $SOURCE_HISTORY_ID --dataset-id $ENA_LINKS -g "$GALAXY_SERVER" -a $API_KEY -t $BOT_RESERVE &&
+    python bioblend-scripts/tag_history.py $SOURCE_HISTORY_ID --dataset-id $ENA_LINKS -g "$GALAXY_SERVER" -a $API_KEY -t $BOT_SIGNAL1 &&
     # run the WF
-    planemo -v run $WF_ID "$WORKDIR/$JOB_YML" --history_name "COG-UK $DEST_NAME_SUFFIX" --tags $DEST_TAG --galaxy_url "$GALAXY_SERVER" --galaxy_user_key $API_KEY --engine external_galaxy 2>&1 > /dev/null | grep -o 'GET /api/histories/[^?]*\?' > "$WORKDIR/run_info.txt" &&
+    planemo -v run $WF_ID "$WORKDIR/$JOB_YML" --history_name "COG-UK $DEST_NAME_SUFFIX" --galaxy_url "$GALAXY_SERVER" --galaxy_user_key $API_KEY --engine external_galaxy 2>&1 > /dev/null | grep -o 'GET /api/histories/[^?]*\?' > "$WORKDIR/run_info.txt" &&
     # on successful completion of the WF invocation inform downstream bots
     # by tagging the new history accordingly
     DEST_HISTORY_ID=$(grep -m1 -o 'histories/[^?]*' "$WORKDIR/run_info.txt" | cut -d / -f 2) &&
-    python bioblend-scripts/tag_history.py $DEST_HISTORY_ID -g "$GALAXY_SERVER" -a $API_KEY -t $DEST_BOT_TAGs
+    python bioblend-scripts/tag_history.py $DEST_HISTORY_ID -g "$GALAXY_SERVER" -a $API_KEY -t $DEST_BOT_TAGs &&
+    # mark the source history ENA links dataset as processed
+    python bioblend-scripts/tag_history.py $SOURCE_HISTORY_ID --dataset-id $ENA_LINKS -g "$GALAXY_SERVER" -a $API_KEY -t $BOT_SIGNAL3 -r $BOT_SIGNAL1 $BOT_SIGNAL2
 fi
 
 rm -R $WORKDIR
