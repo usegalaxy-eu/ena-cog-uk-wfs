61da09a80d0fbcabd7cc87fa849e1607170a5aa0
Simon Bray <sbray1371@gmail.com>
2021-04-08 12:35:56 +0200

    modify ftp_links script to insert galaxy_ids

--- bioblend-scripts/ftp_links_to_yaml.py
+++ bioblend-scripts/ftp_links_to_yaml.py
@@ -6,8 +6,27 @@ import yaml
 
 from bioblend import galaxy
 
+TERMINAL_STATES = {
+    'ok', 'empty', 'error', 'discarded', 'failed_metadata', 'paused'
+}
 
-def parse_ena_fastq_ftp_links(ena_links):
+def upload_dataset(gi, link, upload_attempts):
+    while upload_attempts > 0:
+        dataset_id = gi.tools.put_url(content=link, history_id='d0edc84d344a1239')[outputs][0]['id']
+        for _ in range(100):
+            state = gi.datasets.show_dataset(dataset_id)['state']
+            if state not in TERMINAL_STATES:
+                time.sleep(10)
+                continue
+            else:
+                if state == 'ok':
+                    return dataset_id
+                else:
+                    break
+        upload_attempts -= 1
+
+
+def parse_ena_fastq_ftp_links(ena_links, gi, upload_attempts=100):
     records = {}
     pe_indicator_mapping = {'1': 'forward', '2': 'reverse'}
     is_pe_data = None
@@ -15,6 +34,7 @@ def parse_ena_fastq_ftp_links(ena_links):
         path, file = link.rsplit('/', maxsplit=1)
         ena_id, file_suffix = file.split('.', maxsplit=1)
         link = 'ftp://' + link
+        dataset_id = upload_dataset(gi, link, upload_attempts)
         if is_pe_data is None:
             # Use the first link to decide wether we are dealing with
             # SE or PE data links.
@@ -30,9 +50,9 @@ def parse_ena_fastq_ftp_links(ena_links):
             ena_id, pe_indicator = ena_id.split('_')
             if ena_id not in records:
                 records[ena_id] = {}
-            records[ena_id][pe_indicator_mapping[pe_indicator]] = link
+            records[ena_id][pe_indicator_mapping[pe_indicator]] = dataset_id
         else:
-            records[ena_id] = link
+            records[ena_id] = dataset_id
     return records
 
 
@@ -51,12 +71,12 @@ def records_to_yml_dict(records):
                     {
                         'class': 'File',
                         'identifier': 'forward',
-                        'location': links['forward']
+                        'galaxy_id': links['forward']
                     },
                     {
                         'class': 'File',
                         'identifier': 'reverse',
-                        'location': links['reverse']
+                        'galaxy_id': links['reverse']
                     }
                 ]
             } for record_id, links in records.items()
@@ -67,7 +87,7 @@ def records_to_yml_dict(records):
             {
                 'class': 'File',
                 'identifier': record_id,
-                'location': link
+                'galaxy_id': link
             } for record_id, link in records.items()
         ]
     return yml_dict
@@ -109,7 +129,7 @@ if __name__ == '__main__':
         args.dataset_id
     ).decode("utf-8").splitlines()[1:]
     yaml = records_to_yaml(
-        parse_ena_fastq_ftp_links(ena_links),
+        parse_ena_fastq_ftp_links(ena_links, gi),
         args.collection_name
     )
     if args.output:
