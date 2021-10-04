312aefe5701d7db2d03ed3e1e441f9418d36d104
Kaivan Kamali <kxk302@psu.edu>
2021-10-04 09:29:00 -0400

    Revised code based on code review

--- decompress.py
+++ decompress.py
@@ -1,16 +1,15 @@
 import argparse
-import itertools
 import json
 
 from compress_for_dashboard import parse_compressed
 
 if __name__ == "__main__":
   ap = argparse.ArgumentParser()
-  ap.add_argument("-i", "--input", help="Name of the file to decompress", required=True)
-  ap.add_argument("-o", "--output", help="Name of the decompressed file", required=True)
-  args =vars( ap.parse_args())
+  ap.add_argument("input", help="Name of the file to decompress", required=True)
+  ap.add_argument("output", help="Name of the decompressed file", required=True)
+  args = ap.parse_args()
 
-  with open(args['input'], "r") as in_f, open(args['output'], "w") as out_f:
+  with open(args.input, "r") as in_f, open(args.output], "w") as out_f:
     compressed_data = json.load(in_f)
   
     # A row has the following columns
@@ -18,15 +17,9 @@ if __name__ == "__main__":
     header = "Sample\tPOS\tREF\tALT\tAF\tEFFECT\tCODON\tTRID\tAA\n"
     out_f.write(header)
 
-    rows = list(parse_compressed(compressed_data))
-    for row in rows:
+    rows_gen = parse_compressed(compressed_data)
+    for row in rows_gen:
       sample = row[0]
-      pos = row[1][0]
-      ref = row[1][1]
-      alt = row[1][2]
-      af = row[1][3]
-      effect = row[1][4]
-      codon = row[1][5]
-      trid = row[1][6]
+      pos, ref, alt, af, effect, codon, trid = row[1]
       aa = row[2]
-      out_f.write(str(sample) + "\t" + str(pos) + "\t" + str(ref) + "\t" + str(alt) + "\t" + str(af) + "\t" + str(effect) + "\t" + str(codon) + "\t" + str(trid) + "\t" + str(aa) + "\n")
+      out_f.write('\t'.join([sample, str(pos), ref, alt, str(af), effect, codon, trid, aa]) + '\n'))
