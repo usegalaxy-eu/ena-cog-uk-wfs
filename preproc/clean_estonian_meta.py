dc3e36aca1a15522f60d2ac548d6672015ed36fc
Wolfgang Maier <maierw@informatik.uni-freiburg.de>
2021-12-22 17:27:22 +0100

    Add preprocessing for EE data, update base script

---
+++ preproc/clean_estonian_meta.py
@@ -0,0 +1,14 @@
+import clean_ena_meta
+
+
+def prepend_batch_id(s):
+    yield 'batch_id\t' + s.header
+    for record_line in s:
+        batch_id = 'EST' + record_line.split('\t')[s.col_lookup['library_name']]
+        yield batch_id + '\t' + record_line
+
+if __name__ == '__main__':
+    args = clean_ena_meta.parser.parse_args()
+
+    with open(args.ifile) as i, open(args.ofile, 'w') as o:
+        _ = clean_ena_meta.main(i, o, args.ll, args.ls, args.check_fastqs, mod_func=prepend_batch_id)
