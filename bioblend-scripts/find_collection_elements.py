7e4608d61746238bd937f6e063a12f6b168ca67d
Wolfgang Maier <maierw@informatik.uni-freiburg.de>
2022-11-10 20:53:30 +0100

    Patch tagged history discovery to get bot running again

--- bioblend-scripts/find_collection_elements.py
+++ bioblend-scripts/find_collection_elements.py
@@ -9,6 +9,21 @@ from find_by_tags import (
 from find_datasets import get_matching_datasets_from_histories
 
 
+def get_histories_chunk(gi, chunk_size=100):
+    request_template = '{0}/api/histories?limit={1}&offset={{0}}'.format(
+        gi.base_url, chunk_size
+    )
+    offset=0
+    while True:
+        resp = gi.make_get_request(
+            request_template.format(offset)
+        ).json()
+        if not resp:
+            break
+        yield resp
+        offset += chunk_size
+
+
 def get_matching_slices_from_collections(gi, tags, collections, max_matching):
     yield_count = 0
     for collection in collections:
@@ -84,7 +99,9 @@ if __name__ == '__main__':
 
     history_ids = find_histories_by_tags(
         args.history_tags,
-        gi.histories.get_histories()
+        # ugly hack, which only checks the first 100 histories,
+        # but doesn't need a bigger rewrite for now :(
+        next(get_histories_chunk(gi))
     )
     dataset_types = ['dataset_collection']
     collections_matcher = (
